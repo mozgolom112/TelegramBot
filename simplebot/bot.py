67240514f835147629a1f20ff84fd9a94a40ba73
Nikita Golovanov <cell-26@yandex.ru>
2020-03-04 16:50:01 +0300

    Move simplebot from rep Python

---
+++ simplebot/bot.py
@@ -0,0 +1,32 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(bot, update):
+    mytext = 'Привет, {}! Я простой бот и понимаю только команду {}'.format(update.message.chat.last_name, '/start')
+    update.message.reply_text(mytext)
+
+def chat(bot, update):
+    text = update.message.text
+    update.message.reply_text('Привет!')
+    logging.info(text)
+
+def main():
+    updater = Updater(settings.TELEGRAM_API_KEY)
+    
+    updater.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+    
+    updater.start_polling()
+    updater.idle()
+    
+if __name__ == "__main__":
+    logging.info('Bot started')
+    main()
